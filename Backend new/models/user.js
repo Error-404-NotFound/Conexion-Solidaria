c975d42056ea7d9db65047673de17fa685bef698
Ashish <ashishraj0793@gmail.com>
2024-10-08 20:04:06 +0530

    New test backend

---
+++ Backend new/models/user.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    // email: {
+    //     type: String,
+    //     required: true,
+    //     unique: true
+    // },
+    googleId: {
+        type: String,
+        unique: true
+    }
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+
+
+// UserSchema.statics.findOrCreate = async function (profile, cb) {
+//     try {
+//         const user = await this.findOne({ googleId: profile.id });
+//         // if (!user) {
+//         //     // If no user is found, create a new one
+//         //     user = await this.create({
+//         //         googleId: profile.id,
+//         //         username: profile.displayName,
+//         //         email: profile.emails[0].value   // Google OAuth2 provides the email in the profile
+//         //     });
+//         // }
+//         return cb(null, user);
+//     } catch (err) {
+//         return cb(err, null);
+//     }
+// };
+
+
+module.exports = mongoose.model('User', UserSchema);
