a2146d4c7e840beacebd2b52e39e3ae115251809
Johri Aniket Manish <johrianiket04@gmail.com>
2024-11-11 19:07:22 +0530

    website deployed to vercel bug fix

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000', // Your API base URL
+    baseURL: 'https://conexion-solidaria-backend.vercel.app', // Your API base URL
     withCredentials: true,  // Always send cookies with requests
 });
 
