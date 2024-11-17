c4ae0db9171231fd139e23f364be4eb41e697be9
Ashishraj003 <116660150+Ashishraj003@users.noreply.github.com>
2024-11-17 14:23:36 +0530

    Update About.jsx images addressing

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -16,7 +16,7 @@ const ProfileCard = () => {
       {/* Profile Image and Name */}
       <div className="flex flex-col items-center">
         <img
-          src="/public/pphoto.png"
+          src="/pphoto.png"
           alt="Profile"
           className="w-24 h-24 rounded-full mb-3"
         />
@@ -28,30 +28,30 @@ const ProfileCard = () => {
       <div className="mt-6 space-y-4">
         {/* Phone */}
         <div className="flex items-center space-x-3">
-          <img src="/public/phone.png" alt="Phone" className="w-6 h-6" />
+          <img src="/phone.png" alt="Phone" className="w-6 h-6" />
           <span>7069774750</span>
         </div>
 
         {/* Email */}
         <div className="flex items-center space-x-3">
-          <img src="/public/email.png" alt="Email" className="w-6 h-6" />
+          <img src="/email.png" alt="Email" className="w-6 h-6" />
           <span>cs22b028@iittp.ac.in</span>
         </div>
 
         {/* Date */}
         <div className="flex items-center space-x-3">
-          <img src="/public/calender.png" alt="Calendar" className="w-6 h-6" />
+          <img src="/calender.png" alt="Calendar" className="w-6 h-6" />
           <span>01/09/2024</span>
         </div>
 
         {/* Amounts */}
         <div className="flex justify-between mt-4">
           <div className="flex items-center space-x-3">
-            <img src="/public/income.png" alt="Amount" className="w-6 h-6" />
+            <img src="/income.png" alt="Amount" className="w-6 h-6" />
             <span>Rs. 20000</span>
           </div>
           <div className="flex items-center space-x-3">
-            <img src="/public/donate.png" alt="Donate" className="w-6 h-6" />
+            <img src="/donate.png" alt="Donate" className="w-6 h-6" />
             <span>Rs. 100</span>
           </div>
         </div>
@@ -59,13 +59,13 @@ const ProfileCard = () => {
         {/* Additional Details */}
         <div className="flex justify-between items-center mt-4">
           <div className="flex items-center space-x-3">
-            <img src="/public/badge1.png" alt="Badge" className="w-11 h-11" />
-            <img src="/public/badge2.png" alt="Badge" className="w-11 h-11" />
+            <img src="/badge1.png" alt="Badge" className="w-11 h-11" />
+            <img src="/badge2.png" alt="Badge" className="w-11 h-11" />
           </div>
         </div>
         
         <div className="flex items-center space-x-3">
-          <img src="/public/idk.png" alt="Gallery" className="w-6 h-6" />
+          <img src="/idk.png" alt="Gallery" className="w-6 h-6" />
           <span>04</span>
         </div>
       </div>
