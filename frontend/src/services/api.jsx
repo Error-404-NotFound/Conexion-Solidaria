a82d71289fda140282f1abc41e6573c0d9345857
Ashishraj003 <116660150+Ashishraj003@users.noreply.github.com>
2024-11-17 12:25:56 +0530

    Update api.jsx for Deployment

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://conexion-solidaria-backend.vercel.app', // Your API base URL
-    baseURL: 'http://localhost:3000', // Your API base URL
+     baseURL: 'https://conexion-solidaria-backend.vercel.app', // Your API base URL
+    //baseURL: 'http://localhost:3000', // Your API base URL
     withCredentials: true,  // Always send cookies with requests
 });
 
